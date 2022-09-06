a0b1ee361eedeacdd71b178b2a2487b7dc5fa776
Mac <mac@Macs-MacBook-Air.local>
2022-09-06 15:43:37 +0400

    first commit

---
+++ code4.py
@@ -0,0 +1,10 @@
+
+while 1==1:
+    print("If you want to cancel type exit.")
+    website = input("Enter website name: ")
+    if website == "exit":
+        break
+    print("https://" + website + ".com")
+
+ 
+
